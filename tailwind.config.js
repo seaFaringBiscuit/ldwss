50dbe032d6d5366890d5e28ef748a7231949d960
MichaelESaunders <qwertyui>
2022-10-25 14:53:35 -0400

    Added DaisyUI with theme, navbar, and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        ldwss: {        
+          "primary": "#22d3ee",
+          "secondary": "#2dd4bf",
+          "accent": "#f9a8d4",
+          "neutral": "#374151",
+          "base-100": "#1f2937",
+          "success": "#35D477",
+          "warning": "#F0BA33",
+          "error": "#E35F75",
+        },
+      },
+    ],
+  },
   content: [
     "./src/**/*.{html,js}",
-    "./pages/**/.{html,js}"
-],
+    "./pages/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
