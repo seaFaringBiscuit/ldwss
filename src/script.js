6d7b20ba2795c02f5a2b390f1cc347c061994fde
MichaelESaunders <qwertyui>
2022-10-26 14:57:55 -0400

    ACTUAL HUGE UPDATE LMAOOO

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+    console.log("a")
+    Obs.observe(EL, obsOptions);
+  });
